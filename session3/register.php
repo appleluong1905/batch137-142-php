75a9886de97695f828af86b68a924ff9c4e676d0
Chad Luong <apple.luong1905@gmail.com>
2019-06-04 19:11:23 +0700

    update

--- session3/register.php
+++ session3/register.php
@@ -11,11 +11,14 @@
 <body>
 	<h1>Register form</h1>
 	<?php 
-		$errUserName = $errPassword = '';
+		$errUserName = $errPassword = $errGender = $errCity = '';
 		$username = $password = '';
 		if (isset($_POST['register'])) {
 			$username =  $_POST['username'];
 			$password =  $_POST['password'];
+			$gender   =  $_POST['gender'];
+			$city     =  $_POST['city'];
+			var_dump($_POST['city']);
 			if ($username == '') {
 				$errUserName  = 'Please input your username';
 			} else {
@@ -26,17 +29,42 @@
 			} else {
 				$errPassword = '';
 			}
+			if ($gender == NULL) {
+				$errGender  = 'Please choose your gender';
+			} else {
+				$errGender = '';
+			}
+			if ($city == '') {
+				$errCity  = 'Please choose your city';
+			} else {
+				$errCity = '';
+			}
 		}
 	?>
 	<form action="#" method="POST">
 		<p>Username: 
 			<input type="text" name="username" value="<?php echo $username?>">
-			<p class="error"><?php echo $errUserName;?></p>
 		</p>
+		<p class="error"><?php echo $errUserName;?></p>
 		<p>Password: 
 			<input type="password" name="password" value="<?php echo $password?>">
-			<p class="error"><?php echo $errPassword;?></p>
 		</p>
+		<p class="error"><?php echo $errPassword;?></p>
+		<p>Gender:
+			<input type="radio" name="gender" value="male"> Male
+			<input type="radio" name="gender" value="female"> Female
+			<input type="radio" name="gender" value="other"> Other
+		</p>
+		<p class="error"><?php echo $errGender;?></p>
+		<p>City:
+		<select name="city">
+			<option value="">Please choose city</option>
+			<option value="danang">Da Nang</option>
+			<option value="hanoi">Ha Noi</option>
+			<option value="hochiminh">Ho Chi Minh</option>
+		</select>
+		</p>
+		<p class="error"><?php echo $errCity;?></p>
 		<p>
 			<input type="submit" name="register" value="Register">
 		</p>
