803a6b3aeb434d0d5c03fdf107e0f113fcb50034
Chad Luong <apple.luong1905@gmail.com>
2019-06-27 18:53:32 +0700

    update

--- session8/bt_mvc/batch137_142.sql
+++ session8/bt_mvc/batch137_142.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 25, 2019 at 06:52 PM
+-- Generation Time: Jun 27, 2019 at 06:53 PM
 -- Server version: 5.7.26-0ubuntu0.16.04.1
 -- PHP Version: 7.2.18-1+ubuntu16.04.1+deb.sury.org+1
 
@@ -23,11 +23,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, 'Mod');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(10) NOT NULL,
+  `role_id` int(10) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `gender` varchar(255) NOT NULL,
@@ -39,16 +60,23 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `gender`, `city`, `avatar`) VALUES
-(7, 'canh', '96fddc1d238e5b8a9a5fa1e2dd3bd296', 'female', 'danang', '5d08df6e9e872_girlxinh.jpg'),
-(10, 'admin', 'f4e80f7212cfab35170f2a0b8e6de38a', 'male', 'quangtri', 'default.png'),
-(11, 'john', '15de21c670ae7c3f6f3f1f37029303c9', 'female', 'quangtri', '5d0b885933bb4_kaka.jpg'),
-(12, 'admin2', 'bcbe3365e6ac95ea2c0343a2395834dd', 'male', 'quangtri', '5d0b89ff2dcda_hahaa2.jpg'),
-(13, 'admin5', '78af5f4b34026371672fdb4390267c19', 'female', 'quangtri', '5d0b8b1e4a4bb_oke.jpg');
+INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `gender`, `city`, `avatar`) VALUES
+(7, 0, 'canh', '96fddc1d238e5b8a9a5fa1e2dd3bd296', 'female', 'danang', '5d08df6e9e872_girlxinh.jpg'),
+(11, 0, 'john', '15de21c670ae7c3f6f3f1f37029303c9', 'female', 'quangtri', '5d0b885933bb4_kaka.jpg'),
+(12, 0, 'admin2', 'bcbe3365e6ac95ea2c0343a2395834dd', 'male', 'quangtri', '5d0b89ff2dcda_hahaa2.jpg'),
+(13, 0, 'admin5', '78af5f4b34026371672fdb4390267c19', 'female', 'quangtri', '5d0b8b1e4a4bb_oke.jpg'),
+(17, 0, 'john', 'test', 'male', 'hue', 'test'),
+(18, 0, 'canh', 'test', 'male', 'hue', 'test');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +89,15 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
